ab1c17ce2286c2484b40875c02276f8cf85df983
Moe Abushawish <Abushawish.m@gmail.com>
2020-07-20 15:58:21 -0400

    Support signing multiple release files (#1)

--- src/io-utils.ts
+++ src/io-utils.ts
@@ -1,13 +1,14 @@
 import {Dirent} from "fs";
 import fs from "fs";
 
-export function findReleaseFile(releaseDir: string): Dirent | undefined {
+export function findReleaseFile(releaseDir: string): Dirent[] | undefined {
     const releaseFiles = fs.readdirSync(releaseDir, {withFileTypes: true})
         .filter(item => !item.isDirectory())
         .filter(item => item.name.endsWith(".apk") || item.name.endsWith(".aab"));
 
+    console.log("Found " + releaseFiles.length + " release files.")
+
     if (releaseFiles.length > 0) {
-        return releaseFiles[0]
+        return releaseFiles
     }
 }
-
--- src/main.ts
+++ src/main.ts
@@ -14,35 +14,35 @@ async function run() {
 
     console.log(`Preparing to sign key @ ${releaseDir} with signing key`);
 
-    // 1. Find release file
-    const releaseFile = io.findReleaseFile(releaseDir);
-    if (releaseFile !== undefined) {
-      core.debug(`Found release to sign: ${releaseFile.name}`);
-
-      // 3. Now that we have a release file, decode and save the signing key
+    // 1. Find release files
+    const releaseFiles = io.findReleaseFile(releaseDir);
+    if (releaseFiles !== undefined) {
+      // 3. Now that we have a release files, decode and save the signing key
       const signingKey = path.join(releaseDir, 'signingKey.jks');
       fs.writeFileSync(signingKey, signingKeyBase64, 'base64');
 
-      // 4. Now zipalign the release file
-      const releaseFilePath = path.join(releaseDir, releaseFile.name);
-      let signedReleaseFile = '';
-      if (releaseFile.name.endsWith('.apk')) {
-        signedReleaseFile = await signApkFile(releaseFilePath, signingKey, alias, keyStorePassword, keyPassword);
-      } else if (releaseFile.name.endsWith('.aab')) {
-        signedReleaseFile = await signAabFile(releaseFilePath, signingKey, alias, keyStorePassword, keyPassword);
-      } else {
-        core.error('No valid release file to sign, abort.');
-        core.setFailed('No valid release file to sign.');
+      // 4. Now zipalign and sign each one of the the release files
+      for (let releaseFile of releaseFiles) {
+        core.debug(`Found release to sign: ${releaseFile.name}`);
+        const releaseFilePath = path.join(releaseDir, releaseFile.name);
+        let signedReleaseFile = '';
+        if (releaseFile.name.endsWith('.apk')) {
+          signedReleaseFile = await signApkFile(releaseFilePath, signingKey, alias, keyStorePassword, keyPassword);
+        } else if (releaseFile.name.endsWith('.aab')) {
+          signedReleaseFile = await signAabFile(releaseFilePath, signingKey, alias, keyStorePassword, keyPassword);
+        } else {
+          core.error('No valid release file to sign, abort.');
+          core.setFailed('No valid release file to sign.');
+        }
+
+        core.debug('Release signed! Setting outputs.');
+        core.exportVariable("SIGNED_RELEASE_FILE", signedReleaseFile);
+        core.setOutput('signedReleaseFile', signedReleaseFile);
       }
-
-      console.log('Release signed!');
-      core.debug('Release signed! Setting outputs');
-
-      core.exportVariable("SIGNED_RELEASE_FILE", signedReleaseFile);
-      core.setOutput('signedReleaseFile', signedReleaseFile);
+      console.log('Releases signed!');
     } else {
-      core.error("No release file (.apk or .aab) could be found. Abort.");
-      core.setFailed('No release file (.apk or .aab) could be found.');
+      core.error("No release files (.apk or .aab) could be found. Abort.");
+      core.setFailed('No release files (.apk or .aab) could be found.');
     }
   } catch (error) {
     core.setFailed(error.message);
