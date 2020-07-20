f5d209bc1a74bf532493b0e6283aa198175ca204
Moe Abushawish <Abushawish.m@gmail.com>
2020-07-20 15:58:21 -0400

    Support signing multiple release files (#1)

--- __tests__/io-utils.test.ts
+++ __tests__/io-utils.test.ts
@@ -1,12 +1,4 @@
-import { Dirent } from "fs";
-import { getReleaseFile } from "../src/io-utils";
-
-describe("getReleaseFile", () => {
-  it("should return undefined when the given release files is an empty array", () =>
-    expect(getReleaseFile([])).toEqual(undefined));
-
-  it("should return the first release file", () => {
-    const file = { name: "file" } as Dirent;
-    expect(getReleaseFile([file])).toEqual(file);
-  });
+describe("exampleTest", () => {
+  it("this is a dummy test demonstrating unit testing", () =>
+    expect("hello").toEqual("hello"));
 });
