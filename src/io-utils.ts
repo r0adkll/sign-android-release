4f58d166b751b15b8279584f76b75277baeb665c
Louis Hache <louis.hache@infarm.com>
2020-11-13 19:11:55 +0100

    bump jest and make tests pass

--- src/io-utils.ts
+++ src/io-utils.ts
@@ -1,13 +1,17 @@
 import {Dirent} from "fs";
 import fs from "fs";
 
+export function getReleaseFile(files: Dirent[]): Dirent | undefined {
+    if (files.length > 0) {
+        return files[0]
+    }
+}
+
 export function findReleaseFile(releaseDir: string): Dirent | undefined {
     const releaseFiles = fs.readdirSync(releaseDir, {withFileTypes: true})
         .filter(item => !item.isDirectory())
         .filter(item => item.name.endsWith(".apk") || item.name.endsWith(".aab"));
 
-    if (releaseFiles.length > 0) {
-        return releaseFiles[0]
-    }
+    return getReleaseFile(releaseFiles)
 }
 
