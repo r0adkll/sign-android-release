ce9d6a2f510412db82cb402552aec5bfa432a595
David Wheatley <hi@davwheat.dev>
2020-11-16 15:32:30 +0000

    Fix typo in test description; format test file

--- __tests__/io-utils.test.ts
+++ __tests__/io-utils.test.ts
@@ -1,10 +1,12 @@
-import {Dirent} from "fs";
-import {getReleaseFile} from '../src/io-utils'
+import { Dirent } from "fs";
+import { getReleaseFile } from "../src/io-utils";
 
-describe('getReleaseFile', () => {
-    it('should return undefined when the given release files is an mepty array', () => expect(getReleaseFile([])).toEqual(undefined))
-    it('should return the first release file', () => {
-        const file = {name: 'file'} as Dirent
-        expect(getReleaseFile([file])).toEqual(file)
-    })
-})
+describe("getReleaseFile", () => {
+  it("should return undefined when the given release files is an empty array", () =>
+    expect(getReleaseFile([])).toEqual(undefined));
+
+  it("should return the first release file", () => {
+    const file = { name: "file" } as Dirent;
+    expect(getReleaseFile([file])).toEqual(file);
+  });
+});
